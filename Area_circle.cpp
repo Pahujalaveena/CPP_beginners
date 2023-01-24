c7a7a00cddd807bc386a277fa9335f291d2a784e
Pahujalaveena <113932098+Pahujalaveena@users.noreply.github.com>
2023-01-24 18:00:33 +0000

    Add files via upload

---
+++ Area_circle.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  double pi = 3.14 ;
+  double radius = 8.9 ;      
+  double area = pi * radius * radius;
+  cout << "The Area of the given Circle is ";
+  cout << area;
+  
+  return 0;
+}
