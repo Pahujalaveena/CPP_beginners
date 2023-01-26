25ac3914b23ad45de9a688ca185c5cb151abe82a
Pahujalaveena <113932098+Pahujalaveena@users.noreply.github.com>
2023-01-26 15:27:37 +0000

    Add files via upload

---
+++ Area_rectangle.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int length = 76 ;
+  int width = 45 ;
+  int area = length * width ;
+  cout << "The Area of the given rectangle is " ;
+  cout << area ; 
+  return 0;
+}
