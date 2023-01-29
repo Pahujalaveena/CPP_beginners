7a007687aed8f4221781918cc0abbf99b929457a
Pahujalaveena <113932098+Pahujalaveena@users.noreply.github.com>
2023-01-29 16:20:43 +0000

    Add files via upload

---
+++ Boolean.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  bool x = true ;
+  bool y = false ;
+  cout <<"x="<< x << endl;    //x and y need to be cout on separate lines
+  cout<<"y="<<y;
+  return 0;
+}
