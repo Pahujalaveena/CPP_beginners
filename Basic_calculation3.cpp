fc31a8137ea9fe991e0cb178e6fbb649abde2254
Pahujalaveena <113932098+Pahujalaveena@users.noreply.github.com>
2023-01-29 16:20:17 +0000

    Add files via upload

---
+++ Basic_calculation3.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a = -50 ;
+  int b =40 ;
+  int sum = a + b;
+  int product = a*b ;
+  int quotient = a/b;
+  cout <<"Sum="<< sum << endl;
+  cout << "Product="<<product << endl;
+  cout <<"Quotient"<< quotient;
+  
+  return 0;
+}
