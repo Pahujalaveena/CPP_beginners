a9ae1e2cdf0f374ecc41d20bfce7087b205a628c
Pahujalaveena <113932098+Pahujalaveena@users.noreply.github.com>
2023-02-05 17:06:32 +0000

    Add files via upload

---
+++ If_else.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int Age = 25 ;
+  int Vage = 18;
+
+  if (Age < Vage)  
+  {
+    cout << "Not old enough to vote." << endl;
+    cout << "Wait for " << (Vage - Age) << " years";
+  }
+    else 
+    cout << "Old enough to vote!";
+  
+  return 0;
+}
